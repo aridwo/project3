7bc13eb01d96668e26a26b3f253261dffb956b62
Arielle Dworkin <arielle_dworkin@hks.harvard.edu>
2014-10-26 20:30:54 -0400

    fixed invalid markup

--- app/views/randomuser.blade.php
+++ app/views/randomuser.blade.php
@@ -1,7 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-<meta charset="utf-8" />
-<body>
 @extends('_master')
 @section('title')
 Random User Generator
@@ -34,5 +30,3 @@ Random User Generator
 </p>
 @endforeach
 @stop
-</body>
-</html>
