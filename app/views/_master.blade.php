87909d2a7710dfdb1ff3a73c400e599bd1a54d0c
Arielle Dworkin <arielle_dworkin@hks.harvard.edu>
2014-10-26 21:06:54 -0400

    fix invalid markup

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-<head>
-<title>@yield('title','Project 3')</title>
+<!DOCTYPE html>
+<html>
+<head><title>@yield('title','Project 3')</title>
+
 <meta charset="utf-8" />
+
 {{ HTML::style('style.css') }}
- @yield('head')
-</head>
+	
+@yield('head')</head>
+
 <body>
-<a href="/"><h2>Developer's Best Friend</h2></a><hr>
+	<a href="/"><h2>Developer's Best Friend</h2></a><hr>
 @yield('content')
 </body>
 </html>
